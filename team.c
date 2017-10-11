d4f8834fdb0228c89cf92dd51c61f9ebe3066b18
arellanoemilio <arellanoemilio@gmail.com>
2017-10-11 16:42:27 -0400

    added my name to team file

--- team.c
+++ team.c
@@ -3,8 +3,8 @@
 /* Please be sure to fill in this struct right away! */
 struct team_t team =
 {
-	"", /* first member name   */
-	"", /* first member email  */
+	"Emilio Arellano", /* first member name   */
+	"ema218@lehigh.edu", /* first member email  */
 	"", /* second member name  */
 	"", /* second member email */
 	"", /* third member name   */
