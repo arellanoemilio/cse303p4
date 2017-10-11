2d8b5ab6d4d1f629d1352c8794c00f569222ea30
cam418 <cam418@lehigh.edu>
2017-10-11 16:48:11 -0400

    added cam418

--- team.c
+++ team.c
@@ -5,8 +5,8 @@ struct team_t team =
 {
 	"Emilio Arellano", /* first member name   */
 	"ema218@lehigh.edu", /* first member email  */
-	"", /* second member name  */
-	"", /* second member email */
+	"Charles Mandala", /* second member name  */
+	"cam418@lehigh.edu", /* second member email */
 	"", /* third member name   */
 	"", /* third member email  */
 };
