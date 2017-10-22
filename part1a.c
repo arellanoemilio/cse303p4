bfba3d47255e7a7236c2321de44c02c07e63fc38
arellanoemilio <arellanoemilio@gmail.com>
2017-10-22 16:35:29 -0400

    done part1a

--- part1a.c
+++ part1a.c
@@ -6,9 +6,9 @@
 /* help() - Print a help message. */
 void help(char *progname)
 {
-	printf("Usage: %s\n", progname);
-	printf("(This is just a demonstration of the traditional way\n");
-	printf(" to use .so filed)\n");
+        printf("Usage: %s\n", progname);
+        printf("(This is just a demonstration of the traditional way\n");
+        printf(" to use .so filed)\n");
 }
 
 /* declare the hello() function that lives in a shared library */
@@ -18,27 +18,32 @@ extern void *ucase(void* param);
 /* main() - The main routine parses arguments and invokes hello */
 int main(int argc, char **argv)
 {
-	/* for getopt */
-	long opt;
+        /* for getopt */
+        long opt;
 
-	/* run a student name check */
-	check_team(argv[0]);
+        /* run a student name check */
+        check_team(argv[0]);
 
-	/* parse the command-line options. For this program, we only support */
-	/* the parameterless 'h' option, for getting help on program usage.  */
-	while((opt = getopt(argc, argv, "h")) != -1)
-	{
-		switch(opt)
-		{
-		case 'h': 	help(argv[0]); 	break;
-		}
-	}
+        /* parse the command-line options. For this program, we only support */
+        /* the parameterless 'h' option, for getting help on program usage.  */
+        while((opt = getopt(argc, argv, "h")) != -1)
+        {
+                switch(opt)
+                {
+                case 'h':       help(argv[0]);  break;
+                }
+        }
 
-	hello(NULL);
+        hello(NULL);
 
-	/* TODO: execute the new function "ucase" that you added to libpart1.c */
-	struct team_t *myNewTeam = (struct team_t*) ucase((void *) &team);
-	printf("%s", myNewTeam->name1);
+        /* TODO: execute the new function "ucase" that you added to libpart1.c */
+        struct team_t *myNewTeam = (struct team_t*) ucase((void *) &team);
+        printf("Student 1: %s\n", myNewTeam->name1);
+        printf("Email 1: %s\n", myNewTeam->email1);
+        printf("Student 2: %s\n", myNewTeam->name2);
+        printf("Email 2: %s\n", myNewTeam->email2);
+        printf("Student 3: %s\n", myNewTeam->name3);
+        printf("Email 3: %s\n", myNewTeam->email3);
 
-	return 0;
+        return 0;
 }
